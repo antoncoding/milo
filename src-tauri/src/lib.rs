e1fd791e944a6eb3070deca5536e86a6d94a5618
antoncoding <antonassocareer@gmail.com>
2025-01-15 17:46:25 +0800

    feat: register shortcut

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,7 @@ mod tray;
 mod api;
 
 use tauri::{Manager};
-
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
 use settings::Settings;
 use state::AppState;
 
@@ -20,6 +20,31 @@ pub fn run() {
         .setup(|app| {
             println!("Starting Milo app...");
             let _tray = tray::create_tray_menu(app)?;
+            
+            // Register global shortcut (Cmd+M)
+            #[cfg(desktop)]
+            {
+                let shortcut = Shortcut::new(Some(Modifiers::META), Code::KeyM);  // Using META for Command key
+                app.handle().plugin(
+                    tauri_plugin_global_shortcut::Builder::new()
+                        .with_handler(move |_app, shortcut_ref, event| {
+                            if shortcut_ref == &shortcut {
+                                match event.state() {
+                                    ShortcutState::Pressed => {
+                                        println!("Shortcut Cmd+M pressed!");
+                                    }
+                                    ShortcutState::Released => {
+                                        println!("Shortcut Cmd+M released!");
+                                    }
+                                }
+                            }
+                        })
+                        .build(),
+                )?;
+
+                app.global_shortcut().register(shortcut)?;
+            }
+            
             Ok(())
         })
         .manage(app_state)
